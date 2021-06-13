d48d6269dbce628946b60e26a83ae572f1d6428d
oksana12127 <tulubtsos@gmail.com>
2021-06-13 20:47:35 +0300

    Добавлено приложение для списков с намеренно неработающим модульным тестом

---
+++ func.py
@@ -0,0 +1,13 @@
+import fire
+
+
+class Calculator(object):
+
+    def sum(a):
+        a = 1
+        # b = 2
+        sum = a + 2
+        return print(sum)
+
+if __name__ == '__main__':
+     fire.Fire(Calculator)
