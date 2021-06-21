da276dd7ee0e843a109d7c58ab7b8cc71afdfb6f
oksana12127 <tulubtsos@gmail.com>
2021-06-21 14:50:56 +0300

    ФТ теперь проверяет возможность ввести элемент списка

--- func.py
+++ func.py
@@ -1,12 +1,25 @@
-import fire
+# import fire
 
 
 class Calculator(object):
 
     def sum(self, a):
-
+        print(a)
         sum = a + 2
         return print(sum)
 
+    def minus(self, a):
+        print(a)
+        minus = a - 2
+        return print(minus)
+
+
+
 if __name__ == '__main__':
-    fire.Fire(Calculator)
+    calculator = Calculator()
+    calculator.sum(10)
+    calculator.minus(10)
+
+# fire.Fire(sum)
+
+
--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 # browser = webdriver.Firefox()
@@ -17,7 +19,24 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8222')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
         print(777, self.browser.title)
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
         self.fail('Закончить тест!')
 
 if __name__ == '__main__':
