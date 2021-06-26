0391e363effbbfe0aee86bdd5d92b2ccef0c65e1
oksana12127 <tulubtsos@gmail.com>
2021-06-26 21:27:39 +0300

    Рефакторизовано представления домашней страницы для использования шаблона

--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    '''домашняя страница'''
+    return render(request, 'home.html')
