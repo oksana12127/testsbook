9d55b477a771dfc3500044910060f8296bac3a3b
oksana12127 <tulubtsos@gmail.com>
2021-07-03 17:33:57 +0300

    token

--- functional_tests.py
+++ functional_tests.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+
+
 # browser = webdriver.Firefox()
 # browser.get('http://localhost:8222')
 # assert 'Django' in browser.title
@@ -20,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8222')
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        print(777, self.browser.title)
+        # print(777, self.browser.title)
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -30,6 +32,11 @@ class NewVisitorTest(unittest.TestCase):
         )
         inputbox.send_keys('Купить павлиньи перья')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
@@ -37,11 +44,21 @@ class NewVisitorTest(unittest.TestCase):
         # self.assertTrue(
         #     any(row.text == '1: Купить павлиньи перья' for row in rows)
         # )
-        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows),
-                        "Новый элемент списка не появился в таблице")
+        # self.assertTrue(
+        #     any(row.text == '1: Купить павлиньи перья' for row in rows),
+        #     f"Новый элемент списка не появился в таблице. Содержимым было:"
+        #     f"\n{table.text}"
+        # )
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn(
+            '2: Сделать мушку из павлиньих перьев',
+            [row.text for row in rows]
+        )
         self.fail('Закончить тест!')
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
-
--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+
 from .views import home_page
 
 class HomePageTest(TestCase):
@@ -32,8 +33,21 @@ class HomePageTest(TestCase):
     #
     #     self.assertTemplateUsed(response, 'home.html')
 
+    # def test_uses_home_template(self):
+    #     '''тест домашней страницы'''
+    #     '''тест: используется домашний шаблон'''
+    #     response = self.client.get('/')
+    #     self.assertTemplateUsed(response, 'home.html')
+
     def test_uses_home_template(self):
-        '''тест домашней страницы'''
         '''тест: используется домашний шаблон'''
+
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        '''тест: можно сохранить post-запрос'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
