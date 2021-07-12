caef490bb5eb833f40d741f90f63d874cc38d8c0
oksana12127 <tulubtsos@gmail.com>
2021-07-12 22:56:19 +0300

    сделать functional_tests приложением, использовать LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -11,12 +12,21 @@ import unittest
 # # assert 'To-Do' in browser.title
 # browser.quit()
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
+    '''тест нового посетителя'''
+
     def setUp(self):
+        '''установка'''
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
+
+
+
+
+
+
 
     def check_for_row_in_list_table(self, row_text):
         '''подтверждение строки в таблице списка'''
@@ -26,7 +36,8 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8222')
+        self.browser.get(self.live_server_url)
+        # self.browser.get('http://localhost:8222')
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         # print(777, self.browser.title)
@@ -67,11 +78,12 @@ class NewVisitorTest(unittest.TestCase):
             '2: Сделать мушку из павлиньих перьев',
             [row.text for row in rows]
         )
+
         self.fail('Закончить тест!')
 
 
 
 
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+# if __name__ == '__main__':
+#     unittest.main(warnings='ignore')
